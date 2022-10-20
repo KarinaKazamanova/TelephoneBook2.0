dc7c1f60ebb26de2370fad53a5f14dc664926b75
Karina <k5524392@gmail.com>
2022-10-20 23:25:38 +0300

    обавление возможности работать с сохраненным ранее словарем (нужно правильно записать данные из телефонной книжки в словарь)

--- getdata.py
+++ getdata.py
@@ -2,3 +2,8 @@ def get_data():
     with open('file.txt', 'r', encoding="utf-8") as file:
         name, phone, action = file.readline().split(';')
         return (name, phone, action)  # Нужен картеж
+
+def get_book():
+    with open('telephonebook.txt', 'r', encoding="utf-8") as tb:
+        book = dict(tb.readline().split()) # Надо переделать
+        return book
--- init.py
+++ init.py
@@ -1,3 +1,5 @@
+import getdata as gd
+
 name = ''
 phone = ''
 what_to_do = ''
@@ -9,3 +11,18 @@ def init(n, p, wtd):
     name = n
     phone = p
     what_to_do = wtd
+
+
+def add_new_contact(n, p):
+    t_book = gd.get_book()
+    if n[0] not in t_book:
+        note = (n[0], n, p)
+        t_book = dict(note)
+    return t_book
+name, phone, what_to_do = gd.get_data()
+# n, p, wtd = init(name, phone, what_to_do)
+t_book = add_new_contact(name, phone)
+print(t_book)
+
+note = add_new_contact(name, phone)
+print(note, name[0])
