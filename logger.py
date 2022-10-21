d0ed02f458e882f78cabea6faa4ce70077fd886e
Karina <k5524392@gmail.com>
2022-10-21 16:16:35 +0300

    logger

--- logger.py
+++ logger.py
@@ -3,14 +3,14 @@ import csv
 def writing_down_csv(book: dict):
     with open('names.csv', 'w', newline='') as csvfile:
         for k, v in book.items():
-            csvfile.writelines(f"{k}{v}")
+            csvfile.writelines(f"{k}; {v}")
             csvfile.writelines('\n')
 
 
 def writing_down_txt(book: dict):
     with open('telephonebook.txt', 'w', encoding="utf-8") as file:
         for k, v in book.items():
-            file.writelines(f"{k}{v}")
+            file.writelines(f"{k}; {v}")
             file.writelines('\n')
 
 
